1bf6af0a366579e2d43de39d27d06e1d03ea02a7
Yiğit Bekir Kaya <yigit353@gmail.com>
2023-09-21 07:33:24 +0300

    thquad full split

--- sentiment-analysis/scripts/compare_predictions.sh
+++ sentiment-analysis/scripts/compare_predictions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ../src/combine_predictions.py \
+python3 ../src/utils/combine_predictions.py \
   --validation-file="../datasets/kaggle-movie/folds/5folds/raw/validation_0.csv" \
   --prediction-file-1="../datasets/kaggle-movie/tmp/results/predict_results_bert2d_128k.txt" \
   --prediction-file-2="../datasets/kaggle-movie/tmp/results/predict_results_berturk_128k.txt" \
